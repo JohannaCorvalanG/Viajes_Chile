3547436884127a18c86f3a409323b6d5ee4b93ce
Johanna <johannacorvalang@gmail.com>
2022-01-23 20:31:08 -0300

    Arreglos en el diseño y añado codigo javascript para el boton

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+
+  $("#enviarCorreo").click(function (){
+      alert("El correo fue enviado correctamente...")
+  })
+
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
